406e68966fac7be55431bc4d9b81830d282f4d5c
Shujon Mollah <smollah@theorchard.com>
2015-03-28 17:18:50 -0400

    Use mkmf to check for preexisting installation

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -4,13 +4,24 @@ describe Chromedriver::Helper do
   let(:helper) { Chromedriver::Helper.new }
 
   describe "#binary_path" do
+    context "preexisting installation" do
+      before { allow(helper).to receive(:preexisting_installation) { "/usr/local/bin/chromedriver" } }
+      it { expect(helper.binary_path).to match(/chromedriver$/) }
+    end
+
     context "on a linux platform" do
-      before { allow(helper).to receive(:platform) { "linux32" } }
+      before do
+        allow(helper).to receive(:preexisting_installation).and_return(nil)
+        allow(helper).to receive(:platform) { "linux32" }
+      end
       it { expect(helper.binary_path).to match(/chromedriver$/) }
     end
 
     context "on a windows platform" do
-      before { allow(helper).to receive(:platform) { "win" } }
+      before do
+        allow(helper).to receive(:preexisting_installation).and_return(nil)
+        allow(helper).to receive(:platform) { "win" }
+      end
       it { expect(helper.binary_path).to match(/chromedriver\.exe$/) }
     end
   end

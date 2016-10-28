1082e6cec510dfe3115ee3da1309e949f86b5490
Jon Kaplan <jonandrewkaplan@gmail.com>
2016-10-28 09:34:39 -0700

    Allow the version to be set
    - Add ability to configure version
    - Store the version in a `.chromedriver-version` file and use that for
    which version to run

--- lib/chromedriver/helper.rb
+++ lib/chromedriver/helper.rb
@@ -6,37 +6,69 @@ require 'open-uri'
 require 'archive/zip'
 
 module Chromedriver
-  class Helper
+  def self.root
+    File.dirname __dir__
+  end
 
-    def run *args
+  def self.set_version(version)
+    Chromedriver::Helper.new.update(version)
+  end
+
+  class Helper
+    def run(*args)
       download
       exec binary_path, *args
     end
 
     def download(hit_network = false)
-      return if File.exists?(binary_path) && !hit_network
-      url = download_url
-      filename = File.basename url
+      return if File.exist?(binary_path) && !hit_network
+
+      raise "Version not found for #{download_version}" unless download_url
+
+      filename = File.basename download_url
       Dir.chdir platform_install_dir do
         FileUtils.rm_f filename
         File.open(filename, "wb") do |saved_file|
-          URI.parse(url).open("rb") do |read_file|
+          URI.parse(download_url).open("rb") do |read_file|
             saved_file.write(read_file.read)
           end
         end
-        raise "Could not download #{url}" unless File.exists? filename
+
+        raise "Could not download #{download_url}" unless File.exists? filename
         Archive::Zip.extract(filename, '.', :overwrite => :all)
       end
       raise "Could not unzip #{filename} to get #{binary_path}" unless File.exists? binary_path
       FileUtils.chmod "ugo+rx", binary_path
+      File.open(version_path, 'w') { |file| file.write(download_version) }
     end
 
-    def update
-      download true
+    def update(version = nil)
+      if version
+        @download_version = version
+      end
+
+      hit_network = (current_version != download_version) ? true : false
+      download(hit_network)
+    end
+
+    def current_version
+      @current_version ||= FileUtils.touch(version_path) && File.read(version_path).strip
+    end
+
+    def download_version
+      @download_version ||= current_version || google_code_parser.newest_download_version
     end
 
     def download_url
-      GoogleCodeParser.new(platform).newest_download
+      @download_url ||= google_code_parser.version_download_url(download_version)
+    end
+
+    def google_code_parser
+      @google_code_parser ||= GoogleCodeParser.new(platform)
+    end
+
+    def version_path
+      @version_path ||= File.join(File.expand_path("..", Chromedriver.root), ".chromedriver-version")
     end
 
     def binary_path
@@ -54,7 +86,7 @@ module Chromedriver
     end
 
     def install_dir
-      dir = File.expand_path File.join(ENV['HOME'], ".chromedriver-helper")
+      dir = File.expand_path File.join(ENV['HOME'], ".chromedriver-helper", download_version)
       FileUtils.mkdir_p dir
       dir
     end
@@ -68,6 +100,5 @@ module Chromedriver
       else "win"
       end
     end
-
   end
 end
